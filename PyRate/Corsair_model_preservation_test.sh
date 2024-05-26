db0917c13a49cf4074e0470890478b16c6a07055
Alexis-Marion <95286015+Alexis-Marion@users.noreply.github.com>
2024-05-26 19:50:16 +0200

    Update Corsair_model_preservation_test.sh

--- PyRate/Corsair_model_preservation_test.sh
+++ PyRate/Corsair_model_preservation_test.sh
@@ -13,7 +13,7 @@ while [ ! -f $4/result_boxplot.pdf ]; do
 		echo "$cntr round of optimization"
 		echo "python PyRate/PyRate.py  $1 -j \$1 -qShift $3 -mG -PPmodeltest > $4/File\$1.log" > $4/tmp_script.sh # PyRate temporary script
 		parallel -j $2 bash $4/tmp_script.sh ::: {1..10} # Run parallel
-		bash script.sh $cntr $4
+		bash script_pm.sh $cntr $4
 		Rscript model_drafting.r $4/tab.tsv $3 $4
 		rm $4/tab.tsv
 		rm $4/tmp_script.sh
