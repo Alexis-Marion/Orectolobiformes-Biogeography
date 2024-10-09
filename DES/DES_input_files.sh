70d0d2a5bbab67138fed81ae57fbab6f3ad97565
Alexis-Marion <alexis.marion@umontpellier.fr>
2024-10-09 10:32:39 +0200

    update files

--- DES/DES_input_files.sh
+++ DES/DES_input_files.sh
@@ -4,10 +4,10 @@
 
 # 1 Myrs
 
-python ../PyRate/PyRateDES.py -fossil Input/Sp_extinct_out_Tethys.txt -recent Input/Sp_extant_out_Tethys.txt -wd Analysis_ML/sp_1/ -filename sp_1_DES -bin_size 1 -rep 100
+python ../PyRate/PyRateDES.py -fossil Input/Sp_extinct_Tethys.txt -recent Input/Sp_extant_Tethys.txt -wd Analysis_ML/sp_1/ -filename sp_1_DES -bin_size 1 -rep 20
 
 # GN
 
 # 1 Myrs
 
-python ../PyRate/PyRateDES.py -fossil Input/Gen_extinct_out_Tethys.txt -recent Input/Gen_extant_out_Tethys.txt -wd Analysis_ML/gen_1/ -filename gen_1_DES -bin_size 1 -rep 100
+python ../PyRate/PyRateDES.py -fossil Input/Gen_extinct_Tethys.txt -recent Input/Gen_extant_Tethys.txt -wd Analysis_ML/gen_1/ -filename gen_1_DES -bin_size 1 -rep 20
